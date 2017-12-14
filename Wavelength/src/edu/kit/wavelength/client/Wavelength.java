76be92d9388be8619e884972153f065f22d8b550
Jean-Pierre <jpvdheydt@googlemail.com>
2017-12-14 14:55:58 +0100

    Wavelength Projekt für Entwurf aufgesetzt.

---
+++ Wavelength/src/edu/kit/wavelength/client/Wavelength.java
@@ -0,0 +1,12 @@
+package edu.kit.wavelength.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Wavelength implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+
+	}
+
+}

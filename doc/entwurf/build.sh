3b9304170acafdd973d1e3496d607585d7e9e60d
Markus Himmel <markus@himmel-villmar.de>
2017-12-14 15:58:03 +0100

    Finish up basic functionality for design document (Need to make it look nice)

--- doc/entwurf/build.sh
+++ doc/entwurf/build.sh
@@ -4,13 +4,16 @@ javadoc -docletpath doclet/TeXDoclet.jar \
 	-doclet org.stfm.texdoclet.TeXDoclet \
 	-hyperref \
 	-output build/doclet/out.tex \
-	-sourcepath ../../src/src \
+	-sourcepath ../../Wavelength/src \
 	-subpackages edu \
-	-sectionlevel section \
+	-sectionlevel subsection \
 	-docclass article \
 	-texinit preamble.tex \
+	-texintro intro.tex \
+	-texfinish finish.tex \
+	-texsetup setup.tex \
 	-noindex
 
 latexmk -pdf -auxdir=build/latex -outdir=build/latex build/doclet/out.tex
 
-cp build/latex/out.pdf .
+cp build/latex/out.pdf ./entwurf.pdf

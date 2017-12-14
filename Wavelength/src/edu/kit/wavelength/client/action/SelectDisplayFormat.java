76e492dab1c848922ffbda677751890355e9a37c
tschuelia <julaschmid@googlemail.com>
2017-12-14 19:56:04 +0100

    add action classes

---
+++ Wavelength/src/edu/kit/wavelength/client/action/SelectDisplayFormat.java
@@ -0,0 +1,11 @@
+package edu.kit.wavelength.client.action;
+
+public class SelectDisplayFormat implements Action {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Wavelength/src/edu/kit/wavelength/client/action/ShowSolution.java
@@ -0,0 +1,11 @@
+package edu.kit.wavelength.client.action;
+
+public class ShowSolution implements Action {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

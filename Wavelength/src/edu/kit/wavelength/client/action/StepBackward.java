e59bc792537fcea9f82339ffd9138084ba5f2e86
tschuelia <julaschmid@googlemail.com>
2017-12-16 19:05:49 +0100

    add references to forgotten action class

--- Wavelength/src/edu/kit/wavelength/client/action/StepBackward.java
+++ Wavelength/src/edu/kit/wavelength/client/action/StepBackward.java
@@ -2,11 +2,15 @@ package edu.kit.wavelength.client.action;
 
 import edu.kit.wavelength.client.UIState;
 import edu.kit.wavelength.client.model.ExecutionEngine;
+import edu.kit.wavelength.client.view.OutputDisplayer;
+import edu.kit.wavelength.client.view.ReductionControl;
 
 public class StepBackward implements Action {
 	
 	private ExecutionEngine execution;
 	private UIState state;
+	private OutputDisplayer output;
+	private ReductionControl buttons;
 
 	@Override
 	public void run() {

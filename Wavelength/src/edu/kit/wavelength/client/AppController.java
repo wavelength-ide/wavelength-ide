22a19c9a7e7fef76c22f00afc4f97cbdca4cc955
tschuelia <julaschmid@googlemail.com>
2017-12-21 18:34:45 +0100

    add methods to AppController

--- Wavelength/src/edu/kit/wavelength/client/AppController.java
+++ Wavelength/src/edu/kit/wavelength/client/AppController.java
@@ -1,5 +1,43 @@
 package edu.kit.wavelength.client;
 
-public class AppController implements UIState {
+import edu.kit.wavelength.client.state.AppState;
 
+/**
+ * This class handles the current state of the application. 
+ */
+public class AppController implements UIState {
+	
+	private AppState state;
+
+	public AppController() {
+		// state = new Input();
+	}
+	
+	public void start() {
+		state.start();
+	}
+	
+	public void stop() {
+		state.stop();
+	}
+	
+	public void pause() {
+		state.pause();
+	}
+	
+	public void enterExercise() {
+		state.enterExercise();
+	}
+	
+	public void exitExercise() {
+		state.exitExercise();
+	}
+	
+	public void enterExport() {
+		state.enterExercise();
+	}
+	
+	public void exitExport() {
+		state.exitExercise();
+	}
 }

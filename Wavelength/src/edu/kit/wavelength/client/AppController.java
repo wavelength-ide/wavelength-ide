692e8e75ad7a8c92639834ea9fce992f7f7cc8fc
tschuelia <julaschmid@googlemail.com>
2017-12-21 23:01:32 +0100

    minor changes

--- Wavelength/src/edu/kit/wavelength/client/AppController.java
+++ Wavelength/src/edu/kit/wavelength/client/AppController.java
@@ -3,40 +3,41 @@ package edu.kit.wavelength.client;
 import edu.kit.wavelength.client.state.AppState;
 
 /**
- * This class handles the current state of the application. 
+ * This class handles the current state of the application. The initial state is
+ * the Input state.
  */
 public class AppController implements UIState {
-	
+
 	private AppState state;
 
 	public AppController() {
 		// state = new Input();
 	}
-	
+
 	public void start() {
 		// state.start();
 	}
-	
+
 	public void stop() {
 		// state.stop();
 	}
-	
+
 	public void pause() {
 		// state.pause();
 	}
-	
+
 	public void enterExercise() {
 		// state.enterExercise();
 	}
-	
+
 	public void exitExercise() {
 		// state.exitExercise();
 	}
-	
+
 	public void enterExport() {
 		// state.enterExercise();
 	}
-	
+
 	public void exitExport() {
 		// state.exitExercise();
 	}
--- Wavelength/src/edu/kit/wavelength/client/Wavelength.java
+++ Wavelength/src/edu/kit/wavelength/client/Wavelength.java
@@ -4,11 +4,18 @@ import com.google.gwt.core.client.EntryPoint;
 
 import edu.kit.wavelength.client.state.AppState;
 
+/**
+ * This class marks the entry point of the application.
+ */
 public class Wavelength implements EntryPoint {
-	
+
+	/**
+	 * This method is called when the application is first started. It initializes
+	 * the applications controller class.
+	 */
 	@Override
 	public void onModuleLoad() {
 		// initialize UIState
 	}
-		
+
 }

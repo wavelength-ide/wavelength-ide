a2470ca50e2a3fed78f30496a1b344e27e194d27
Markus Himmel <markus@himmel-villmar.de>
2017-12-29 13:42:05 +0100

    Minor: Remove a missed TODO

--- Wavelength/src/edu/kit/wavelength/client/model/library/Boolean.java
+++ Wavelength/src/edu/kit/wavelength/client/model/library/Boolean.java
@@ -10,19 +10,16 @@ final class Boolean implements Library {
 
 	@Override
 	public LambdaTerm getTerm(String name) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean containsName(String name) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 

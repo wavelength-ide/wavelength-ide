258a8121face22bd78222cb526a99ed85711a22f
tschuelia <julaschmid@googlemail.com>
2018-01-04 10:04:25 +0100

    javadoc for AppController class

--- Wavelength/src/edu/kit/wavelength/client/AppController.java
+++ Wavelength/src/edu/kit/wavelength/client/AppController.java
@@ -6,57 +6,82 @@ import edu.kit.wavelength.client.model.term.LambdaTerm;
 import edu.kit.wavelength.client.state.AppState;
 
 /**
- * This class handles the current state of the application. The initial state is
- * the Input state.
+ * This class handles the current state of the application and the currently
+ * displayed output. The initial state is the Input state and the output is
+ * empty when the application is started.
  */
 public class AppController implements UIState {
 
 	private AppState state;
 	private List<LambdaTerm> currentOutput;
 
+	/**
+	 * Constructs a new AppController object. The initial state is Input and the
+	 * output is set empty.
+	 */
 	public AppController() {
 		// state = new Input();
 	}
 
+	/**
+	 * This method gets called when the reduction process starts. It delegates the
+	 * handling to the current state.
+	 */
 	public void start() {
 		// state.start();
 	}
 
+	/**
+	 * This method gets called when the reduction process stops. It delegates the
+	 * handling to the current state.
+	 */
 	public void stop() {
 		// state.stop();
 	}
 
+	/**
+	 * This method gets called when the reduction process is paused. It delegates
+	 * the handling to the current state.
+	 */
 	public void pause() {
 		// state.pause();
 	}
 
+	/**
+	 * This method gets called when an exercise is selected. It delegates the
+	 * handling to the current state.
+	 */
 	public void enterExercise() {
 		// state.enterExercise();
 	}
 
+	/**
+	 * This method gets called when the user exits the exercise mode. It delegates
+	 * the handling to the current state.
+	 */
 	public void exitExercise() {
 		// state.exitExercise();
 	}
 
-	public void enterExport() {
-		// state.enterExercise();
-	}
-
-	public void exitExport() {
-		// state.exitExercise();
-	}
-
+	/**
+	 * Adds a lambda term to the current output.
+	 */
 	@Override
 	public void addTerm(LambdaTerm term) {
 		// add term to end of currentOutput
-		
 	}
 
+	/**
+	 * Removes the last lambda term from the current output.
+	 */
 	@Override
 	public void popTerm() {
-		// pop last term in currentOutput		
+		// pop last term in currentOutput
 	}
 
+	/**
+	 * Empties the current output.
+	 */
 	@Override
 	public void clearOutput() {
 		// currentOutput = new List<LambdaTerm>();

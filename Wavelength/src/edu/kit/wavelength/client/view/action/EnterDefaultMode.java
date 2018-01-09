dde3dd75162f213005c88f0442aeecd160c432da
tschuelia <julaschmid@googlemail.com>
2018-01-09 18:18:17 +0100

    add forgotten action class

---
+++ Wavelength/src/edu/kit/wavelength/client/view/action/EnterDefaultMode.java
@@ -0,0 +1,19 @@
+package edu.kit.wavelength.client.view.action;
+
+import edu.kit.wavelength.client.view.action.Action;
+
+/**
+ * This class changes the view from exercise mode view to standard input view.
+ */
+public class EnterDefaultMode implements Action {
+
+	/**
+	 * Resizes the editor window to full width, hides the solution and task windows
+	 * and hides the buttons for exiting and showing the solution.
+	 */
+	@Override
+	public void run() {
+
+	}
+
+}
--- Wavelength/src/edu/kit/wavelength/client/view/action/ExitExerciseMode.java
+++ Wavelength/src/edu/kit/wavelength/client/view/action/ExitExerciseMode.java
@@ -1,15 +1,15 @@
 package edu.kit.wavelength.client.view.action;
 
 /**
- * This class changes the view from exercise view to standard input view.
+ * This action will try to exit the exercise mode and alerts the user when
+ * contents of the editor would be overwritten. Otherwise it changes the view
+ * from exercise view to standard input view.
  */
 public class ExitExerciseMode implements Action {
 
 	/**
 	 * Opens a popup window if the editor contains code that would be overwritten.
-	 * If this is not the case or the overwriting was accepted, this method resizes
-	 * the editor window to full width, hides the solution and task windows and
-	 * hides the buttons for exiting and showing the solution.
+	 * If this is not the case this method changes the view to standard input view.
 	 */
 	@Override
 	public void run() {

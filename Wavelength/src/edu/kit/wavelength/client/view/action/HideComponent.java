0ae8fab413c794241bdd8c48cf3c5a033e1f3002
Markus Himmel <markus@himmel-villmar.de>
2018-01-09 19:48:50 +0100

    Undo JPs accidental revert

--- Wavelength/src/edu/kit/wavelength/client/view/action/HideComponent.java
+++ Wavelength/src/edu/kit/wavelength/client/view/action/HideComponent.java
@@ -3,24 +3,30 @@ package edu.kit.wavelength.client.view.action;
 import edu.kit.wavelength.client.view.api.Hideable;
 
 /**
- * This action opens the main menu.
+ * This action generalizes interaction with UI components that do nothing but
+ * being shown or hid.
  */
 public class HideComponent implements Action {
 
-	// intended for: using the menu button, using the export button, hiding the solution
-	
+	// intended for: using the menu button, using the export button, hiding the
+	// solution
+
 	private Hideable hideable;
-	
+
 	/**
-	 * Constructs a new OpenMainMenu Action.
+	 * Constructs a new action handler for a UI component thats only purpose is to
+	 * be shown or hid.
 	 * 
-	 * @param menu
-	 *            The Menu that should be shown.
+	 * @param hideable
+	 *            The UI component that is supposed to be hid or shown.
 	 */
 	private HideComponent(Hideable hideable) {
 		this.hideable = hideable;
 	}
 
+	/**
+	 * Hides the given component if currently displayed, displays it otherwise.
+	 */
 	@Override
 	public void run() {
 		if (hideable.isShown()) {

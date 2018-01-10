c550e8c01ef60feebbd4042b230ca57b8c484c4d
Jean-Pierre <jpvdheydt@googlemail.com>
2018-01-10 20:58:17 +0100

    Beschleunigung der natürlichen Zahlen hinzugefügt.

--- Wavelength/src/edu/kit/wavelength/client/model/library/NaturalNumbers.java
+++ Wavelength/src/edu/kit/wavelength/client/model/library/NaturalNumbers.java
@@ -6,8 +6,11 @@ import edu.kit.wavelength.client.model.term.LambdaTerm;
  * A {@link Library} matching integer literals to church numerals
  * and providing functions for basic arithmetic operations.
  *
+ * Note that the {@link ExecutionEngine} can try to
+ * accelerate the calculation on a {@link LambdaTerm}
+ * that uses this library.
  */
-public final class NaturalNumbers implements Library{
+public final class NaturalNumbers implements Library {
 
 	@Override
 	public LambdaTerm getTerm(String name) {

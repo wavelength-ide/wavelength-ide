559eba2ab9ff1fc125a89ddf0484240be2884d57
mhuisi <mhuisi@protonmail.com>
2018-01-19 20:48:39 +0100

    change some editor options

--- Wavelength/src/edu/kit/wavelength/client/view/webui/gwtbinding/MonacoEditor.java
+++ Wavelength/src/edu/kit/wavelength/client/view/webui/gwtbinding/MonacoEditor.java
@@ -1,18 +1,23 @@
 package edu.kit.wavelength.client.view.webui.gwtbinding;
 
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.user.client.ui.Panel;
 
 public class MonacoEditor {
+	
+	private static native JavaScriptObject config() /*-{
+		var c = {
+			"value": "hello word",
+			"automaticLayout": true,
+			"minimap": {"enabled": false},
+			"mouseWheelZoom": true
+		};
+		return c;
+	}-*/;
+	
 	public static native void load(Panel parent) /*-{
 		var id = parent.@com.google.gwt.user.client.ui.Panel::getElement()().@com.google.gwt.dom.client.Element::getId()();
-		var editor = $wnd.monaco.editor.create($doc.getElementById(id), {
-			value: [
-				'function x() {',
-				'\tconsole.log("Hello world!");',
-				'}'
-			].join('\n'),
-			language: 'javascript',
-			automaticLayout: true
-		});
+		var c = @edu.kit.wavelength.client.view.webui.gwtbinding.MonacoEditor::config()();
+		var editor = $wnd.monaco.editor.create($doc.getElementById(id), c);
 	}-*/;
 }

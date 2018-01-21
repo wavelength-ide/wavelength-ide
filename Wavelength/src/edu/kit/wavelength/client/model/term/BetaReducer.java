66ff48b43804abd0a3dbfd18c2b8dae77a84d767
Markus Himmel <markus@himmel-villmar.de>
2018-01-21 11:53:02 +0100

    The BetaReducer class (i.e. the easy part of beta reduction)

--- Wavelength/src/edu/kit/wavelength/client/model/term/BetaReducer.java
+++ Wavelength/src/edu/kit/wavelength/client/model/term/BetaReducer.java
@@ -1,5 +1,7 @@
 package edu.kit.wavelength.client.model.term;
 
+import java.util.Objects;
+
 /**
  * A {@link Visitor} that transforms a {@link LambdaTerm} by beta reducing a given
  * redex.
@@ -15,6 +17,11 @@ public final class BetaReducer extends TermTransformer {
 	 * This must be a redex, otherwise an exception is thrown.
 	 */
 	public BetaReducer(Application toReduce) {
+		Objects.requireNonNull(toReduce);
+		
+		if (toReduce.acceptVisitor(new IsRedexVisitor()) == false)
+			throw new IllegalArgumentException("toReduce must be a redex");
+
 		this.toReduce = toReduce;
 	}
 	
@@ -25,22 +32,26 @@ public final class BetaReducer extends TermTransformer {
 
 	@Override
 	public LambdaTerm visitAbstraction(Abstraction abs) {
-		return null;
+		return new Abstraction(abs.getPreferredName(), abs.getInner().acceptVisitor(this));
 	}
 
 	@Override
 	public LambdaTerm visitApplication(Application app) {
-		return null;
+		if (app != toReduce)
+			return new Application(app.getLeftHandSide().acceptVisitor(this),
+				app.getRightHandSide().acceptVisitor(this));
+			
+		return app.getLeftHandSide().acceptVisitor(new SubstitutionVisitor(-1, app.getRightHandSide()));
 	}
 
 	@Override
 	public LambdaTerm visitBoundVariable(BoundVariable var) {
-		return null;
+		return var;
 	}
 
 	@Override
 	public LambdaTerm visitFreeVariable(FreeVariable var) {
-		return null;
+		return var;
 	}
  
 }

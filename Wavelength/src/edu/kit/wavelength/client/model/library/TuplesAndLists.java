8aeaf0a9ce1c8d798c157bba8f88b296ba5868da
Jean-Pierre <jpvdheydt@googlemail.com>
2018-02-07 17:45:03 +0100

    Listen zu Bibliothek hinzugefügt.

--- Wavelength/src/edu/kit/wavelength/client/model/library/TuplesAndLists.java
+++ Wavelength/src/edu/kit/wavelength/client/model/library/TuplesAndLists.java
@@ -16,11 +16,28 @@ import edu.kit.wavelength.client.model.term.NamedTerm;
  */
 public final class TuplesAndLists implements Library {
 
+	// true and false needed for more advanced list operations
+	private final NamedTerm tru = new NamedTerm("true", new Abstraction("t", new Abstraction("f", new BoundVariable(2))));
+	private final NamedTerm fal = new NamedTerm("false", new Abstraction("t", new Abstraction("f", new BoundVariable(1))));
+	
 	private final NamedTerm pair = new NamedTerm("pair", new Abstraction("x", new Abstraction("y", new Abstraction("z", new Application(new Application(new BoundVariable(1), new BoundVariable(3)), new BoundVariable(2))))));
 	private final NamedTerm first = new NamedTerm("first", new Abstraction("p", new Application(new BoundVariable(1), new Abstraction("x", new Abstraction("y", new BoundVariable(2))))));
 	private final NamedTerm second = new NamedTerm("second", new Abstraction("p", new Application(new BoundVariable(1), new Abstraction("x", new Abstraction("y", new BoundVariable(1))))));
 	
-	private final List<NamedTerm> definitions = Arrays.asList(pair, first, second);
+	// Build a list where each list node is pair (a,b). 
+	// Where 'a' is the head of the list and 'b' the tail (possibly containing more pairs).
+	// A List containing only one Element 'a' should be constructed by: '(prepend a newList)'
+	// same as 'false'
+	private final NamedTerm newList = new NamedTerm("newList", new Abstraction("t", new Abstraction("f", new BoundVariable(1))));
+	// same as 'pair'
+	private final NamedTerm prepend = new NamedTerm("prepend", new Abstraction("x", new Abstraction("y", new Abstraction("z", new Application(new Application(new BoundVariable(1), new BoundVariable(3)), new BoundVariable(2))))));
+	// same as 'first'
+	private final NamedTerm head = new NamedTerm("head", new Abstraction("p", new Application(new BoundVariable(1), new Abstraction("x", new Abstraction("y", new BoundVariable(2))))));
+	// same as 'second'
+	private final NamedTerm tail = new NamedTerm("tail", new Abstraction("p", new Application(new BoundVariable(1), new Abstraction("x", new Abstraction("y", new BoundVariable(1))))));
+	private final NamedTerm isEmpty = new NamedTerm("isEmpty", new Abstraction("l", new Application(new Application(new BoundVariable(1), new Abstraction("h", new Abstraction("t", new Abstraction("d", fal)))), tru)));
+	
+	private final List<NamedTerm> definitions = Arrays.asList(tru, fal, pair, first, second, newList, prepend, head, tail, isEmpty);
 	
 	public static final char ID = 't';
 	

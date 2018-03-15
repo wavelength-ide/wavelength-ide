64220b1536e191bb05a1abb985b0878a8c1752f2
Jean-Pierre <jpvdheydt@googlemail.com>
2018-03-15 17:50:29 +0100

    kleine Test für die zahlen Bibliothek.

--- Wavelength/test/edu/kit/wavelength/client/model/library/NaturalNumbersTest.java
+++ Wavelength/test/edu/kit/wavelength/client/model/library/NaturalNumbersTest.java
@@ -1,5 +1,33 @@
 package edu.kit.wavelength.client.model.library;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class NaturalNumbersTest {
 
+	@Test
+	public void containsValidNumber() {
+		NaturalNumbers library = new NaturalNumbers(false);
+		for(int i = 0; i <= 1000; i++){
+			assertTrue(library.containsName(Integer.toString(i)));
+		}
+		
+		library = new NaturalNumbers(true);
+		for(int i = 0; i <= 1000; i++){
+			assertTrue(library.containsName(Integer.toString(i)));
+		}
+	}
+	
+	@Test
+	public void containsInvalidNumber() {
+		NaturalNumbers library = new NaturalNumbers(false);
+		assertFalse(library.containsName("-1"));
+		assertFalse(library.containsName("00"));
+		assertFalse(library.containsName("008"));
+		assertFalse(library.containsName("a"));
+		assertFalse(library.containsName(""));
+		assertFalse(library.containsName(null));
+	}
 }

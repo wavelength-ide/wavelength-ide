f202e0be54a7bbe6751814bcb27c648114fce014
Max Wagner <max@trollbu.de>
2018-03-23 14:36:47 +0100

    Put exercise data into a separate resource file instead of inline
    Also, add an exercise explaining the Church Booleans and two examples

--- Wavelength/src/edu/kit/wavelength/client/view/exercise/Exercises.java
+++ Wavelength/src/edu/kit/wavelength/client/view/exercise/Exercises.java
@@ -3,6 +3,10 @@ package edu.kit.wavelength.client.view.exercise;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.TextResource;
+
 import edu.kit.wavelength.client.model.reduction.ReductionOrder;
 import edu.kit.wavelength.client.model.reduction.ReductionOrders;
 
@@ -11,6 +15,54 @@ import edu.kit.wavelength.client.model.reduction.ReductionOrders;
  *
  */
 public final class Exercises {
+	interface ExerciseData extends ClientBundle {
+		ExerciseData instance = GWT.create(ExerciseData.class);
+		  
+		@Source("/exerciseData.txt")
+		TextResource exerciseData();
+	}
+	
+
+	private static final String EXERCISE_SEPARATOR_PREFIX = "=====";
+	private static final String SECTION_SEPARATOR_PREFIX = "-----";
+	  
+	/* format of an exercise:
+	 * ============
+	 * My Exercise
+	 * ----------- task description
+	 * In this exercise, define some lambda-terms or something
+	 * ----------- solution
+	 * (\x. x)
+	 * ----------- template text
+	 * (YOUR AD HERE)
+	 */
+	
+	private static boolean isSeparator(String line) {
+		return line.startsWith(EXERCISE_SEPARATOR_PREFIX) || line.startsWith(SECTION_SEPARATOR_PREFIX);
+	}
+  
+	private static ArrayList<Exercise> parseData() {
+		ArrayList<Exercise> exercises = new ArrayList<Exercise>();
+
+		String[] contents = ExerciseData.instance.exerciseData().getText().split("\\n");
+		int i = 0;
+		while (i < contents.length) {
+			// parse Name, Task Description, Solution, and Template Text
+			String[] sections = new String[4];
+			for (int sec = 0; sec < 4; sec++) {
+				assert isSeparator(contents[i]);
+				i++;
+				StringBuilder str = new StringBuilder();
+				while (i < contents.length && !isSeparator(contents[i])) {
+					str.append(contents[i] + "\n");
+					i++;
+				}
+				sections[sec] = str.toString();
+			}
+			exercises.add(new ConcreteExercise(sections[0], sections[1], sections[2], sections[3]));
+		}
+		return exercises;
+	}
 
 	/**
 	 * Returns an unmodifiable list of all available exercises.
@@ -20,101 +72,7 @@ public final class Exercises {
 	 */
 	public static List<Exercise> all() {
 
-		String exerciseName = "Exercise mode";
-		String exerciseTask = "The current exercise's task will be displayed here. "
-				+ " <br/> To show a solution for the current exercise press the lightbulb button to the right.";
-		String exerciseSolution = "To leave the exercise mode press the X button in the upper right corner.";
-		String exercisePredef = "Some exercises will display prewritten terms for you to work with here.";
-		Exercise exerciseMode = new ConcreteExercise(exerciseName, exerciseTask, exerciseSolution, exercisePredef);
-
-		String varName = "Variables";
-		String varTask = "In lambda calculus, bound variables are variables enclosed in the body of an abstraction,"
-				+ " while free variables are those not enclosed by an abstraction."
-				+ "<br/> Note that the same variable may appear as a bound and as a free variable in the same term. "
-				+ "<br/> Which variables in the displayed term are free? " + "<br/> Which are bound?";
-		String varSolution = "The first v, and last x are free variables. " + "<br/> All other variables are bound.";
-		String varPredef = "v ((λx. λv. (v x)) x)";
-		Exercise var = new ConcreteExercise(varName, varTask, varSolution, varPredef);
-
-		String freeVarName = "Variables II";
-		String freeVarTask = "Which variables in the displayed term are bound variables?";
-		String freeVarSolution = "The term's first x, second y, first v and fourth x are bound variables. "
-				+ "<br/> All other variables are free.";
-		String freeVarPredef = "(λx. x y)(λy. y x)((λv. x v) λx. x)";
-		Exercise freeVar = new ConcreteExercise(freeVarName, freeVarTask, freeVarSolution, freeVarPredef);
-
-		String alphaReduName = "α-conversion";
-		String alphaReduTask = "α-conversion enables renaming variables bound by an abstraction with some restrictions "
-				+ "to prevent the term"
-				+ " from changing its structure: "
-				+ "<br/> The renaming must not result in free variables becoming bound ones and only variables bound "
-				+ "to the selected abstraction may be renamed. "
-				+ "<br/> If a term can be transformed into another term only by α-renaming variables, the two terms "
-				+ "are α-equivalent."
-				+ "<br/> Knowing these restrictions which of these terms are α-equivalent to the first one?";
-		String alphaReduSolution = "Only the first and fourth terms are α-equivalent.";
-		String alphaReduPredef = "λx. (λx. y x) z \n" + "λz. (λx. y x) z \n" + "λx. (λy. y y) z \n"
-				+ "λx. (λv. y v) z \n" + "λy.(λx.y x) y \n" + "λx.(λz.y z) o ";
-		Exercise alphaRedu = new ConcreteExercise(alphaReduName, alphaReduTask, alphaReduSolution, alphaReduPredef);
-
-		String order1Name = "Normal Order";
-		String order1Task = "If a λ-term contains multiple reducable expressions the question which to reduce first "
-				+ "is answered by the applied reduction order "
-				+ " one of which is the normal reduction order. "
-				+ "<br/> Using this reduction order the outermost leftmost redex is reduced first. "
-				+ "<br/> In respect to the tree output format this is equivalent to traversing the term-tree according"
-				+ " to a depth-first-search until one reaches a redex."
-				+ "<br/> Which abstraction in the displayed term makes up the left part of the redex to reduce first?"
-				+ "<br/> You can check your answer by selecting normal order and reducing the term using the "
-				+ "Step-Forward-Button.";
-		String order1Solution = "(λz. z) x";
-		String order1Predef = "λx. x((λz. z) x)((λv. v ((λz. x v) z)) j)";
-		Exercise order1 = new ConcreteExercise(order1Name, order1Task, order1Solution, order1Predef);
-
-		String order2Name = "Applicative Order";
-		String order2Task = "Applicative Order is the exact opposite of the normal reduction order: the innermost "
-				+ "rightmost redex is reduced first. "
-				+ "<br/> This means a redex is only reduced when it does not contain any redex itself. "
-				+ "<br/> In respect to tree output no redex is reduced until all redexes in its subtree have been reduced."
-				+ "<br/> Using applicative order which redex in the displayed term will be reduced first?";
-		String order2Solution = "(λv. x v) z  will be reduced first";
-		String order2Predef = "λx. x((λz. v z) x)((λv. v ((λv. x v) z))j)";
-		Exercise order2 = new ConcreteExercise(order2Name, order2Task, order2Solution, order2Predef);
-
-		String order3Name = "Call-by-Name";
-		String order3Task = "Call-by-Name reduction is similar to the normal reduction order: the outermost leftmost "
-				+ "redex is reduced first."
-				+ "<br/> Call-by-Name's major difference is the restriction that only redexes not enclosed in the body"
-				+ " of an abstraction may be reduced."
-				+ "<br/> This means that when using Call-by-Name a term might still contain redexes but will not be "
-				+ "reduced further."
-				+ "<br/> Using Call-by-Name which redex in the displayed term will be reduced first?";
-		String order3Solution = "(λz. ((λv. v x) x) z z)(u v) will be reduced first.";
-		String order3Predef = "(λz. ((λv. v x) x) z z)(u v)(λx. x)u";
-		Exercise order3 = new ConcreteExercise(order3Name, order3Task, order3Solution, order3Predef);
-
-		String order4Name = "Call-by-Value";
-		String order4Task = "Just like Call-by-Name using the Call-by-Value reduction order the outermost leftmost "
-				+ "redex is reduced first and redexes enclosed by an abstraction"
-				+ " will never be reduced but in addition this reduction order demands that the right side of a "
-				+ "redex has to be a value if it is to be reduced."
-				+ "<br/> \"Value\" refers to a lambda term that can not be reduced any further meaning that it is "
-				+ "either a variable or an abstraction."
-				+ "<br/> Using call-by-value which redex in the displayed term will be reduced first?";
-		String order4Solution = "(λx. x)(λz. z x)";
-		String order4Predef = "(λv. v v)(a b)((λx. x)(λz. z x))";
-		Exercise order4 = new ConcreteExercise(order4Name, order4Task, order4Solution, order4Predef);
-
-		ArrayList<Exercise> exerciseList = new ArrayList<Exercise>();
-
-		exerciseList.add(exerciseMode);
-		exerciseList.add(var);
-		exerciseList.add(freeVar);
-		exerciseList.add(order1);
-		exerciseList.add(order2);
-		exerciseList.add(order3);
-		exerciseList.add(order4);
-		exerciseList.add(alphaRedu);
+		ArrayList<Exercise> exerciseList = parseData();
 
 		List<ReductionOrder> reductions = ReductionOrders.all();
 		for (ReductionOrder cOrder : reductions) {

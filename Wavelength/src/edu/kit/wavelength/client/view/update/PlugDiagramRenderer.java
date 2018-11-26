8d18cbea965f4fd1484a162169de12f27069866d
Max Wagner <max@trollbu.de>
2018-11-26 17:15:38 +0100

    make svg output render structurally (mostly) correct

--- Wavelength/src/edu/kit/wavelength/client/view/update/PlugDiagramRenderer.java
+++ Wavelength/src/edu/kit/wavelength/client/view/update/PlugDiagramRenderer.java
@@ -12,6 +12,7 @@ import org.vectomatic.dom.svg.OMSVGLength;
 import org.vectomatic.dom.svg.OMSVGPathElement;
 import org.vectomatic.dom.svg.OMSVGPathSeg;
 import org.vectomatic.dom.svg.OMSVGPathSegList;
+import org.vectomatic.dom.svg.OMSVGRect;
 import org.vectomatic.dom.svg.OMSVGRectElement;
 import org.vectomatic.dom.svg.OMSVGSVGElement;
 import org.vectomatic.dom.svg.OMSVGTextElement;
@@ -30,71 +31,174 @@ import com.google.gwt.user.client.ui.FlowPanel;
 import edu.kit.wavelength.client.model.term.*;
 
 public class PlugDiagramRenderer {
+	private enum LayoutItemType {
+		RECT,
+		ABS,
+		VAR,
+		TEXT,
+		
+		CHEVRON,
+		PACMAN,
+		// ...
+	}
 
 	private static class SVGLayoutElement {
 		public float x;
 		public float y;
 		public float width;
 		public float height;
-		private Class<?> TermType;
-//		public Set<OMSVGElement> elems;
+		public LayoutItemType type;
+		private List<SVGLayoutElement> children;
+		private LambdaTerm term;
+		private String text;
 
-		public void moveTo(int x, int y) {
+		public SVGLayoutElement(String text) {
+			this(LayoutItemType.TEXT);
+			this.text = text;
+			this.width = PlugDiagramRenderer.fontSize * text.length() * 0.6f;
+			this.height = PlugDiagramRenderer.fontSize;
+		}
+
+		public SVGLayoutElement(LayoutItemType type) {
+			this(type, (LambdaTerm) null);
+		}
+		
+		public SVGLayoutElement(LayoutItemType type, LambdaTerm term) {
+			this.children = new ArrayList<>();
+			this.type = type;
+			this.term = term;
+		}
+		public void addChild(SVGLayoutElement elem) {
+			this.children.add(elem);
+		}
+		public void moveTo(float x, float y) {
 			this.x = x;
 			this.y = y;
 		}
 		
-		public void translate(int x, int y) {
+		/**
+		 * propagates relative positions through the tree, turning relative x-y-coordinates to absolute ones
+		 */
+		public void finalizeLayout() {
+			for (SVGLayoutElement child : children) {
+				child.translate(this.x, this.y);
+				child.finalizeLayout();
+			}
+			
+		}
+		
+		public void translate(float x, float y) {
 			this.x += x;
 			this.y += y;
 		}
 		
-		public Set<OMSVGElement> render() {
-			// TODO
+		public Set<OMSVGElement> renderAbs() {
+			// m 978.895 193.92
+			// a rx      ry      angle 0 0 dx       dy
+			// a 14.4852 14.4852 0     0 0 -14.4843 14.4844
+			// a 14.4852 14.4852 0     0 0 14.4843 14.4863
+			// a 14.4852 14.4852 0     0 0 7.4629 -2.08789
+			// l -6.9082 -13.6738
+			// l 5.9688 -11.6406
+			// a 14.4852 14.4852 0     0 0 -6.5235 -1.56836
+			// Z
+
+			OMSVGPathElement pacman = doc.createSVGPathElement();
+			OMSVGPathSegList segs = pacman.getPathSegList();
+			segs.appendItem(pacman.createSVGPathSegMovetoAbs(this.x, this.y));
+			segs.appendItem(pacman.createSVGPathSegArcRel(-14.4843f, 14.4844f, 14.4852f, 14.4852f, 0f, false, false));
+			segs.appendItem(pacman.createSVGPathSegArcRel(14.4843f, 14.4863f, 14.4852f, 14.4852f, 0f, false, false));
+			segs.appendItem(pacman.createSVGPathSegArcRel(7.4629f, -2.08789f, 14.4852f, 14.4852f, 0f, false, false));
+			segs.appendItem(pacman.createSVGPathSegLinetoRel(-6.9082f, -13.6738f));
+			segs.appendItem(pacman.createSVGPathSegLinetoRel(5.9688f, -11.6406f));
+			segs.appendItem(pacman.createSVGPathSegArcRel(-6.5235f, -1.56836f, 14.4852f, 14.4852f, 0f, false, false));
+			segs.appendItem(pacman.createSVGPathSegClosePath());
+
+			Set<OMSVGElement> res = new HashSet<>();
+			res.add(pacman);
+			return res;
+		}
+		public Set<OMSVGElement> renderRect() {
+			// TODO return null;
 			return null;
 		}
-		
-		
-		public void recalc() {
-			left = Float.MAX_VALUE;
-			top = Float.MAX_VALUE;
-			right = Float.MIN_VALUE;
-			bottom = Float.MIN_VALUE;
-			for (OMSVGElement el : elems) {
-				float x = pxToFloat(el.getAttribute("x"));
-				float y = pxToFloat(el.getAttribute("y"));
-				float width = pxToFloat(el.getAttribute("width"));
-				float height = pxToFloat(el.getAttribute("height"));
-				left = Math.min(x,  left);
-				right = Math.max(x+width,  right);
-				top = Math.min(y, top);
-				bottom = Math.max(y+height, bottom);
+		public OMSVGElement renderVar() {
+			// somewhat hacky, but we want bound variables to be basically invisible, but take up a certain amount of space
+			// TODO maybe make a rect instead?
+			return renderText("  ");
+		}
+		public OMSVGElement renderChevron() {
+			OMSVGPathElement chevron = doc.createSVGPathElement();
+			OMSVGPathSegList segs = chevron.getPathSegList();
+            segs.appendItem(chevron.createSVGPathSegMovetoAbs(this.x + this.width, this.y));
+            segs.appendItem(chevron.createSVGPathSegLinetoRel(-this.width, this.height / 2));
+            segs.appendItem(chevron.createSVGPathSegLinetoRel(this.width, this.height / 2));
+            chevron.setAttribute("stroke-width", strokeWidth);
+            chevron.setAttribute("stroke", "#000000");
+            chevron.setAttribute("fill", "none");
+            
+			return chevron;
+		}
+		public Set<OMSVGElement> render() {
+			Set<OMSVGElement> res = new HashSet<>();
+			switch (this.type) {
+			case ABS:
+				res = renderAbs();
+			case TEXT:
+				res.add(renderText(this.text));
+				break;
+			case RECT:
+				res.add(renderRoundedRect());
+				break;
+			case CHEVRON:
+				res.add(renderChevron());
+				break;
+			default:
+				res.add(renderDebugRect(null));
+				break;
 			}
+			for (SVGLayoutElement child : children) {
+				res.addAll(child.render());
+			}
+			return res;
+			
 		}
 		
-		public void union(PartialResult other) {
-			if (this.elems.addAll(other.elems)) {
-				left = Math.min(left, other.left);
-				top = Math.min(top,  other.top);
-				bottom = Math.max(bottom, other.bottom);
-				right = Math.max(right,  other.right);
-			}
+		private OMSVGElement renderText(String text) {
+			OMSVGTextElement elem = doc.createSVGTextElement(x, y+fontSize, OMSVGLength.SVG_LENGTHTYPE_PX, text);
+			elem.setAttribute("font-size", Float.toString(fontSize) + "px");
+			elem.setAttribute("font-family", "monospace");
+			return elem;
+		}
+		
+		private OMSVGElement renderDebugRect(String stroke) {
+			OMSVGRectElement rect = doc.createSVGRectElement(x, y, width, height, 0, 0);
+			rect.setAttribute("stroke", stroke == null ? "#DD0000" : stroke);
+			rect.setAttribute("stroke-width", strokeWidth);
+			rect.setAttribute("fill", "#101010");
+			return rect;
+		}
+		
+		private OMSVGElement renderRoundedRect() {
+			
+			OMSVGRectElement rect = doc.createSVGRectElement(x, y, width, height, 2* height / 5, 2 * height / 5);
+			rect.setAttribute("stroke", "#000000");
+			rect.setAttribute("stroke-width", strokeWidth);
+			rect.setAttribute("fill", "none");
+			return rect;
+		}
+		private float pxToFloat(String attribute) {
+			return Float.parseFloat(attribute.substring(0, attribute.length()-2));
 		}
 
-		private void shift(float x, float y) {
-			this.left += x;
-			this.right += x;
-			this.top += y;
-			this.bottom += y;
-			for (OMSVGElement e : elems) {
-				PlugDiagramRenderer.shift(e, x, y);
-			}
-		}
-	
 	}
 	
-	private static final float fontSize = 25f;
+	private static final float fontSize = 17f;
+	private static final float chevronSharpness = 0.8f;
+	private static final String strokeWidth = "2px";
+	private static final float spacing = 7f;
 	private static OMSVGDocument doc;
+	
 	private static Logger l;
 	
 	public static void renderDiagram(LambdaTerm t, Application nextRedex, Panel target) {
@@ -102,81 +206,105 @@ public class PlugDiagramRenderer {
 		PlugDiagramRenderer.l = java.util.logging.Logger.getLogger("SVGDebug");
 		PlugDiagramRenderer.doc = OMSVGParser.currentDocument();
 		OMSVGSVGElement svg = doc.createSVGSVGElement();
-		OMSVGElement rr = renderRoundedRectAt(0, 0, 120, 50);
+		svg.setAttribute("width", "100%");
 		
-		for (OMSVGElement el : PlugDiagramRenderer.renderApplication((Application) t).elems) {
+		SVGLayoutElement root = PlugDiagramRenderer.layoutLambdaTerm(t);
+		root.translate(spacing, spacing);
+		root.finalizeLayout();
+		
+		for (OMSVGElement el : root.render()) {
 			svg.appendChild(el);
 		}
 		
 		target.getElement().appendChild(svg.getElement());
 	}
 	
-	public static PartialResult renderTextAt(String text, float x, float y) {
-		OMSVGTextElement elem = doc.createSVGTextElement(x, y+fontSize, OMSVGLength.SVG_LENGTHTYPE_PX, text);
-		elem.setAttribute("font-size", Float.toString(fontSize));
-		return elem;
+	
+	private static SVGLayoutElement layoutLambdaTerm(LambdaTerm term) {
+		if (term instanceof Application) return layoutApplication((Application) term);
+		if (term instanceof Abstraction) return layoutAbstraction((Abstraction) term);
+		if (term instanceof BoundVariable) return layoutBoundVariable((BoundVariable) term);
+		if (term instanceof FreeVariable) return layoutFreeVariable((FreeVariable) term);
+		if (term instanceof NamedTerm) return layoutNamedTerm((NamedTerm) term);
 		
-	}
-	
-	public static OMSVGElement renderBoundVariableAt(BoundVariable v, float x, float y) {
-		// somewhat hacky, but we want bound variables to be basically invisible, but take up a certain amount of space
-		// TODO maybe make a rect instead?
-		return renderTextAt("  ", x, y);
-	}
-	
-	public static OMSVGElement renderRoundedRectAt(float x, float y, float width, float height) {
-		OMSVGRectElement rect = doc.createSVGRectElement(x, y, width, height, 2* height / 5, 2 * height / 5);
-		rect.setAttribute("stroke", "#000000");
-		rect.setAttribute("stroke-width", "2");
-		rect.setAttribute("fill", "none");
-		return rect;
-	}
-	
-	private static void shift(OMSVGElement e, float x, float y) {
-		if (e instanceof OMSVGPathElement) {
-			// path elements don't do x,y attributes. Instead, use transform: translate(x,y)
-			// FIXME TODO
-			//l.log(Level.SEVERE, e.getAttribute("transform"));
-		} else {
-			l.log(Level.SEVERE, e.getAttribute("x"));
-			float oldx = pxToFloat(e.getAttribute("x"));
-			float oldy = pxToFloat(e.getAttribute("y"));
-			e.setAttribute("x", Float.toString(oldx + x));
-			e.setAttribute("y", Float.toString(oldy + y));
-		}
-	}
-	
-	
-	private static PartialResult renderLambdaTerm(LambdaTerm t) {
 		return null;
 	}
 	
-	private static float pxToFloat(String attribute) {
-		return Float.parseFloat(attribute.substring(0, attribute.length()-2));
+	private static SVGLayoutElement layoutBorderedText(String text) {
+		SVGLayoutElement border = new SVGLayoutElement(LayoutItemType.RECT);
+		SVGLayoutElement textElem = new SVGLayoutElement(text);
+		border.width = textElem.width + 2*spacing;
+		border.height = textElem.height + 2*spacing;
+		textElem.translate(spacing, spacing);
+		border.addChild(textElem);
+		return border;
 	}
 	
-	public static PartialResult renderApplication(Application app) {
+	private static SVGLayoutElement layoutNamedTerm(NamedTerm term) {
+		// print like a free variable
+		return layoutBorderedText(term.getName());
+	}
+
+
+	private static SVGLayoutElement layoutFreeVariable(FreeVariable term) {
+		// TODO Auto-generated method stub
+		return new SVGLayoutElement(term.getName());
+	}
+
+
+	private static SVGLayoutElement layoutBoundVariable(BoundVariable term) {
+		// TODO Auto-generated method stub
+		return layoutBorderedText(Integer.toString(term.getDeBruijnIndex()));
+	}
+
+
+	private static SVGLayoutElement layoutAbstraction(Abstraction term) {
+		SVGLayoutElement lam = new SVGLayoutElement("λ" + term.getPreferredName() + ".");
+		SVGLayoutElement body = layoutLambdaTerm(term.getInner());
+		SVGLayoutElement pacman = new SVGLayoutElement(LayoutItemType.PACMAN);
+		
+		lam.translate(0, /* centering */ Math.max(0, (body.height - lam.height)/2f ));
+		body.translate(lam.width, 0);
+		lam.width += body.width;
+		lam.height = Math.max(lam.height, body.height);
+		body.translate(-lam.x, -lam.y);
+		
+		pacman.translate(body.x + body.width, lam.y);
+		lam.width += pacman.width;
+		lam.addChild(pacman);
+		
+		// pretend that lam encloses body
+		lam.addChild(body);
+		return lam;
+	}
+
+
+	public static SVGLayoutElement layoutApplication(Application app) {
+		SVGLayoutElement roundedRect = new SVGLayoutElement(LayoutItemType.RECT, app);
+		
 		LambdaTerm left = app.getLeftHandSide();
-		PartialResult lres = renderLambdaTerm(left);
+		SVGLayoutElement lres = layoutLambdaTerm(left);
+		
 		LambdaTerm right = app.getRightHandSide();
-		PartialResult rres = renderLambdaTerm(right);
-		// float maxheight = Math.max(lres.bottom - lres.top, rres.bottom - rres.top);
+		SVGLayoutElement rres = layoutLambdaTerm(right);
+	
+		float maxheight = Math.max(lres.height, rres.height);
+		lres.translate(spacing, Math.max(0, (maxheight - lres.height) / 2 + spacing));
+		rres.translate(spacing, Math.max(0, (maxheight - rres.height) / 2 + spacing));
 		
-		// that's the height we'll need to scale the chevron to...
-		//OMSVGPathElement chevron = doc.createSVGPathElement();
-		//OMSVGPathSegList segments = chevron.getPathSegList();
-		// TODO
-		//segments.appendItem(chevron.createSVGPathSegMovetoAbs(500, 42));
-		//shift(chevron, 10, 4422);
+		SVGLayoutElement chevron = new SVGLayoutElement(LayoutItemType.CHEVRON);
+		chevron.height = maxheight + 2*spacing; // spacing above and below
+		chevron.width = chevron.height * chevronSharpness / 2;
+		chevron.translate(lres.x + lres.width, 0);
+		rres.translate(chevron.x + chevron.width, 0);
 		
-		Set<OMSVGElement> elems = new HashSet<>(5);
+		roundedRect.width = rres.x + rres.width + spacing;
+		roundedRect.height = chevron.height;
 		
-		elems.add(renderRoundedRectAt(0, 0, 100, 20));
-		OMSVGElement text = renderTextAt("Test", 0, 0);
+		roundedRect.addChild(lres);
+		roundedRect.addChild(chevron);
+		roundedRect.addChild(rres);
 		
-		elems.add(text);
-		PartialResult res = new PartialResult(elems);
-		
-		return res;
+		return roundedRect;
 	}
 }
